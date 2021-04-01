011bd0f7d7cce04f7e7d90096e17cd49801432e0
Chris Jakuc <chris.jakuc@gmail.com>
2021-03-31 20:53:56 -0400

    2nd attempt at static implementation rebuild

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from pages.predictions import predict
 
 # Imports from this application
 from app import app, server
